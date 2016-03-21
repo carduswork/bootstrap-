edd4002a1aa47df8c048cef81fbeed52e03c4b1f
carduswork <1360998812@qq.com>
2016-03-21 10:30:29 +0800

    cancel

--- filecollection/Program.cs
+++ filecollection/Program.cs
@@ -4,35 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections;
 namespace filecollection
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+
             //book lunyu = new book();
             //lunyu.Name = "栈";
             //Stack<book> books = new Stack<book>();
             //books.Push(lunyu);
             //Console.WriteLine(books.Pop().Name);
 
-            //Dictionary<int,string> dict=new Dictionary<int,string>();
-            //dict.Add(2, "字典");
-            //Console.WriteLine(dict[2]);
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            dict.Add(2, "字典");
+            Console.WriteLine(dict[2]);
 
-            //Hashtable ht = new Hashtable();
-            //ht.Add(3, "哈希");
-            //Console.WriteLine(ht[3]);
+            Hashtable ht = new Hashtable();
+            ht.Add(3, "哈希");
+            Console.WriteLine(ht[3]);
 
-            //Console.ReadKey();
-            //books.Clear();
-            //string path = "a.txt";
-            //StreamWriter sw = new StreamWriter(path);
-            //sw.Write("写入");
-            //sw.Close();
-            //StreamReader sr = new StreamReader(path);
-            //Console.WriteLine(sr.ReadLine());
+            Console.ReadKey();
+            books.Clear();
+            string path = "a.txt";
+            StreamWriter sw = new StreamWriter(path);
+            sw.Write("写入");
+            sw.Close();
+            StreamReader sr = new StreamReader(path);
+            Console.WriteLine(sr.ReadLine());
             using (FileStream fs=new FileStream("binary.txt",FileMode.Create))
             {
                 BinaryWriter bw=new BinaryWriter(fs);
